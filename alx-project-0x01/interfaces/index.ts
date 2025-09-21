0d7603bb274814ed7a5ab0d394ef193912c67c58
Gedion47 <gedionalemu279@gmail.com>
2025-09-21 14:42:33 +0300

    updated

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -64,5 +64,6 @@ export interface UserData {
 
 export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (user: UserData) => void;
+  /* onSubmit: (user: UserData) => void; */
+  onSubmit: (post: UserProps) => void;
 }
