cd393ac536dfaa6095fb13a5c25a3e140679ef60
Gedion47 <gedionalemu279@gmail.com>
2025-09-21 14:07:18 +0300

    updated

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
 
 const UsersPage: React.FC = () => {
   const users = ["Alice", "Bob", "Charlie"];
@@ -17,5 +18,14 @@ const UsersPage: React.FC = () => {
     </div>
   );
 };
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
 
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 export default UsersPage;
